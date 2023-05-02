7c446764c88e18fa6cf3901e23dff6bf9a54023b
Juan Diaz <jfdiaza@unal.edu.co>
2023-05-02 02:29:49 -0400

    style app according to required design

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -4,6 +4,6 @@ class Entity < ApplicationRecord
   belongs_to :user
   belongs_to :group
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
   validates :amount, numericality: { only_integer: true, greater_or_equal_to: 0 }
 end
